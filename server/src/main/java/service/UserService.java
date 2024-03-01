80ecc3439325d20b17048493cc36cc07d65c59d3
Ryan Linerud <rinerud@hotmail.com>
2024-02-29 18:50:47 -0700

    Finished Auth service

--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -1,16 +1,57 @@
 package service;
 
+import dataAccess.MemoryDataAccess;
 import model.AuthData;
 import model.UserData;
 
+import java.util.ArrayList;
+import java.util.Objects;
+import java.util.UUID;
+
 public class UserService {
+    private final MemoryDataAccess dataAccess;
+
+    public UserService() {
+        this.dataAccess = new MemoryDataAccess();
+    }
 
     public AuthData register(UserData user) {
-        return null;
+        ArrayList<UserData> cur_users = (ArrayList<UserData>) dataAccess.listUser();
+        if (cur_users.contains(user)){
+            return null;
+        } else {
+            dataAccess.addUser(user);
+            String newToken = createAuth();
+            AuthData newAuth = new AuthData(newToken,user.username());
+            dataAccess.addAuth(newAuth);
+            return newAuth;
+        }
     }
     public AuthData login(UserData user) {
-        return null;
+        ArrayList<UserData> cur_users = (ArrayList<UserData>) dataAccess.listUser();
+        if (cur_users.contains(user)){
+            String newToken = createAuth();
+            AuthData newAuth = new AuthData(newToken,user.username());
+            dataAccess.addAuth(newAuth);
+            return newAuth;
+        }
+        else{
+            return null;
+        }
     }
-    public void logout(UserData user) {}
+    public void logout(UserData user) {
+        ArrayList<AuthData> cur_auth = (ArrayList<AuthData>) dataAccess.listAuth();
+        String toRemove = null;
+        for (AuthData auth : cur_auth){
+            if (Objects.equals(auth.username(), user.username())){
+                toRemove = auth.authToken();
+            }
+        }
+        dataAccess.deleteAuth(toRemove);
+    }
+
+    public String createAuth(){
+        return UUID.randomUUID().toString();
+    }
 
 }
